631b828ad16b180c687d7722ebcf38cf8fec8b6c
sanu <sanu.sathyaseelan@nagarro.com>
2020-09-06 11:41:56 +0400

    Adds new extensions & custom views

---
+++ AxiomTelecom/Sources/Extensions/UIAlertControllerExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIAlertControllerExtensions.swift
+//  AxiomTelecom
+//
+//  Created by Sanu Sathyaseelan on 06/09/2020.
+//  Copyright © 2020 Sanu. All rights reserved.
+//
+
+import Foundation
---
+++ AxiomTelecom/Sources/Utils/LoadingView.swift
@@ -0,0 +1,9 @@
+//
+//  LoadingView.swift
+//  AxiomTelecom
+//
+//  Created by Sanu Sathyaseelan on 06/09/2020.
+//  Copyright © 2020 Sanu. All rights reserved.
+//
+
+import Foundation
